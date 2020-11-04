7f85ddf95062cb954add76a6b8b69d800e5d4808
zbrookle <zachb1996@yahoo.com>
2020-11-03 20:23:59 -0500

    ENH: Finish design for orchestrator

--- cron/cron_orchestrator.go
+++ cron/cron_orchestrator.go
@@ -1,12 +1,32 @@
-package cron
+package cron_test
 
-import batchv1 "k8s.io/api/batch/v1beta1"
+import batch "k8s.io/api/batch/v1beta1"
 
 // Orchestrator holds information for all cronjobs
 type Orchestrator struct {
-	cronMap map[string]batchv1.CronJob
+	cronMap map[string]*batch.CronJob
 }
 
-func addJob(orchestrator Orchestrator, job batchv1.CronJob) {
+// NewOrchestrator creates an empty instance of Orchestrator
+func NewOrchestrator() *Orchestrator {
+	return &Orchestrator{make(map[string]*batch.CronJob)}
+}
+
+// AddJob adds a CronJob object to the Orchestrator
+func (orchestrator Orchestrator) AddJob(job *batch.CronJob) {
 	orchestrator.cronMap[job.ObjectMeta.Name] = job
+}
+
+// Jobs returns a CronJob list
+func (orchestrator Orchestrator) Jobs() []*batch.CronJob {
+	 jobs := make([]*batch.CronJob, 0, len(orchestrator.cronMap))
+	 for job := range orchestrator.cronMap {
+		 jobs = append(jobs, orchestrator.cronMap[job])
+	 }
+	return jobs
+}
+
+// fillJobs fills up the jobs layer with existing scheduled kubernetes jobs
+func (orchestrator Orchestrator) fillJobs() {
+
 }
---
+++ cron/cron_orchestrator_test.go
@@ -0,0 +1,43 @@
+package cron
+
+import (
+	"fmt"
+	batch "k8s.io/api/batch/v1beta1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+)
+
+func CreateCronJob(cronID int) *batch.CronJob {
+	var cronName = fmt.Sprint("cronJob&d", cronID)
+	var kubeType = meta.TypeMeta{Kind: "CronJob", APIVersion: batch.SchemeGroupVersion.Version}
+	var objectMeta = meta.ObjectMeta{Name: cronName}
+	var cronSpec = batch.CronJobSpec{Schedule: "* * * * *", JobTemplate: batch.JobTemplateSpec{}}
+	return &batch.CronJob{TypeMeta: kubeType, ObjectMeta: objectMeta, Spec: cronSpec, Status: batch.CronJobStatus{}}
+}
+
+func TestAddCronJobToOrchestrator(t *testing.T) {
+	var job = CreateCronJob(0)
+	var orch = NewOrchestrator()
+	const expectedLength = 1
+	orch.AddJob(job)
+	if orch.cronMap[job.ObjectMeta.Name] != job {
+		t.Error("CronJob not added at correct key")
+	}
+	if len(orch.cronMap) != expectedLength {
+		t.Errorf("CronMap should have length %d", expectedLength)
+	}
+}
+
+func TestGetCronJobs(t *testing.T) {
+	var cronJobs = []*batch.CronJob{CreateCronJob(0), CreateCronJob(1)}
+	var cronMap = make(map[string]*batch.CronJob)
+	for _, job := range cronJobs {
+		cronMap[job.ObjectMeta.Name] = job
+	}
+	var orch = Orchestrator{cronMap: cronMap}
+	for j, job := range orch.GetJobs() {
+		if cronJobs[j] != job {
+			t.Error("CronJob lists do not match")
+		}
+	}
+}
