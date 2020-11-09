600fe00ee9b2aedd8eda9fc57d01eb23e73ff7c8
zbrookle <zachb1996@yahoo.com>
2020-11-08 21:32:56 -0500

    ENH: Update testing

--- orchestrator/dag_orchestrator_test.go
+++ orchestrator/dag_orchestrator_test.go
@@ -1,131 +1,37 @@
 package orchestrator
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"testing"
+import (
+	"testing"
 
-// 	"goflow/cron"
+	"goflow/dags"
 
-// 	batchv1 "k8s.io/api/batch/v1"
-// 	batchv1beta1 "k8s.io/api/batch/v1beta1"
-// 	core "k8s.io/api/core/v1"
-// 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
-// 	"k8s.io/client-go/kubernetes/fake"
-// )
+	"k8s.io/client-go/kubernetes/fake"
+)
 
-// var kubeClient *fake.Clientset
+var kubeClient *fake.Clientset
 
-// func createFakeKubeClient() *fake.Clientset {
-// 	return fake.NewSimpleClientset()
-// }
+func createFakeKubeClient() *fake.Clientset {
+	return fake.NewSimpleClientset()
+}
 
-// func TestMain(m *testing.M) {
-// 	kubeClient = createFakeKubeClient()
-// 	m.Run()
-// }
+func TestMain(m *testing.M) {
+	kubeClient = createFakeKubeClient()
+	m.Run()
+}
 
-// func CreateCronJob(cronID int) *batchv1beta1.CronJob {
-// 	var cronName = fmt.Sprintf("cron-job-%d", cronID)
-// 	var kubeType = meta.TypeMeta{Kind: "CronJob", APIVersion: batchv1beta1.SchemeGroupVersion.Version}
-// 	var objectMeta = meta.ObjectMeta{Name: cronName}
-// 	var cronSpec = batchv1beta1.CronJobSpec{Schedule: "* * * * *", JobTemplate: batchv1beta1.JobTemplateSpec{Spec: batchv1.JobSpec{Template: core.PodTemplateSpec{
-// 		ObjectMeta: meta.ObjectMeta{
-// 			Labels: map[string]string{
-// 				"app": "demo",
-// 			},
-// 		},
-// 		Spec: core.PodSpec{
-// 			Containers: []core.Container{
-// 				{
-// 					Name:  "web",
-// 					Image: "nginx:1.12",
-// 					Ports: []core.ContainerPort{
-// 						{
-// 							Name:          "http",
-// 							Protocol:      core.ProtocolTCP,
-// 							ContainerPort: 80,
-// 						},
-// 					},
-// 				},
-// 			},
-// 			RestartPolicy: "Never",
-// 		},
-// 	}}}}
-// 	return &batchv1beta1.CronJob{
-// 		TypeMeta:   kubeType,
-// 		ObjectMeta: objectMeta,
-// 		Spec:       cronSpec,
-// 		Status:     batchv1beta1.CronJobStatus{},
-// 	}
-// }
+func TestRegisterDAG(t *testing.T) {
+	dag := dags.NewDAG("test", "default", "* * * * *", "busyboxy", "Never", kubeClient)
+	orch := newOrchestratorFromClient(kubeClient)
+	const expectedLength = 1
+	orch.registerDag(dag)
+	if orch.dagMap[dag.Name] != dag {
+		t.Error("CronJob not added at correct key")
+	}
+	if len(orch.dagMap) != expectedLength {
+		t.Errorf("CronMap should have length %d", expectedLength)
+	}
+}
 
-// func TestRegisterCronJob(t *testing.T) {
-// 	job := CreateCronJob(0)
-// 	orch := newOrchestratorFromClient(kubeClient)
-// 	const expectedLength = 1
-// 	orch.registerDag(job)
-// 	if orch.dagMap[job.ObjectMeta.Name] != job {
-// 		t.Error("CronJob not added at correct key")
-// 	}
-// 	if len(orch.dagMap) != expectedLength {
-// 		t.Errorf("CronMap should have length %d", expectedLength)
-// 	}
-// }
+func TestCollectDags(t *testing.T) {
 
-// func TestCreateCronJobInK8S(t *testing.T) {
-// 	job := CreateCronJob(0)
-// 	orch := newOrchestratorFromClient(kubeClient)
-// 	createdJob := orch.createKubeJob(job)
-
-// 	expectedJobsSet := cron.NewSetFromList([]batchv1beta1.CronJob{*createdJob})
-// 	namespace := "default"
-
-// 	// Retrieve jobs that are present in k8s
-// 	retrievedJobListObject, err := kubeClient.BatchV1beta1().CronJobs(
-// 		namespace,
-// 	).List(
-// 		context.TODO(),
-// 		meta.ListOptions{},
-// 	)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	// Delete test job
-// 	kubeClient.BatchV1beta1().CronJobs(
-// 		namespace,
-// 	).Delete(
-// 		context.TODO(),
-// 		job.Name,
-// 		meta.DeleteOptions{},
-// 	)
-
-// 	retrievedJobSet := cron.NewSetFromList(retrievedJobListObject.Items)
-// 	if !retrievedJobSet.Equals(&expectedJobsSet) {
-// 		t.Errorf(
-// 			"Retrieved job list did not match expected job list. \nExpected: %s\nRetrieved:%s",
-// 			expectedJobsSet.String(),
-// 			retrievedJobSet.String(),
-// 		)
-// 	}
-// }
-
-// func TestGetCronJobs(t *testing.T) {
-// 	var expectedCronJobs = []*batchv1beta1.CronJob{CreateCronJob(0), CreateCronJob(1)}
-// 	var cronMap = make(map[string]*batchv1beta1.CronJob)
-// 	for _, job := range expectedCronJobs {
-// 		cronMap[job.ObjectMeta.Name] = job
-// 	}
-// 	var orch = Orchestrator{dagMap: cronMap}
-// 	for j, foundCronJob := range orch.Jobs() {
-// 		expectedJob := expectedCronJobs[j]
-// 		if expectedJob != foundCronJob {
-// 			t.Errorf(
-// 				"Expected job %s, found job %s",
-// 				cron.GetJobFormattedJSONString(*expectedJob),
-// 				cron.GetJobFormattedJSONString(*foundCronJob),
-// 			)
-// 		}
-// 	}
-// }
+}
