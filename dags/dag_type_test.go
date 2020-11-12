badae3c2131f82031413f868280c8fa04780e9d8
zbrookle <zachb1996@yahoo.com>
2020-11-12 11:15:26 -0500

    CLN: Separate out dag run tests

--- dags/dag_type_test.go
+++ dags/dag_type_test.go
@@ -2,7 +2,6 @@ package dags
 
 import (
 	"context"
-	"goflow/jsonpanic"
 	"goflow/testutils"
 	"path/filepath"
 	"sort"
@@ -11,7 +10,6 @@ import (
 
 	"encoding/json"
 
-	batch "k8s.io/api/batch/v1"
 	core "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -152,56 +150,7 @@ func TestAddDagRun(t *testing.T) {
 	}
 }
 
-func TestCreateJob(t *testing.T) {
-	defer testutils.CleanUpJobs(KUBECLIENT)
-	dagRun := createDagRun(getTestDate(), getTestDag())
-	dagRun.CreateJob()
-	foundJob, err := dagRun.DAG.kubeClient.BatchV1().Jobs(
-		dagRun.DAG.Config.Namespace,
-	).Get(
-		context.TODO(),
-		dagRun.Job.Name,
-		v1.GetOptions{},
-	)
-	if err != nil {
-		panic(err)
-	}
-	foundJobValue := jsonpanic.JSONPanic(*foundJob)
-	expectedValue := jsonpanic.JSONPanic(*dagRun.Job)
-	if foundJobValue != expectedValue {
-		t.Error("Expected:", expectedValue)
-		t.Error("Found:", foundJobValue)
-	}
-}
-
-func unmarshalJob(job batch.Job) string {
-	bytes := make([]byte, 0)
-	err := job.Unmarshal(bytes)
-	if err != nil {
-		panic(err)
-	}
-	return string(bytes)
-}
-
-func TestDeleteJob(t *testing.T) {
-	defer testutils.CleanUpJobs(KUBECLIENT)
-	dagRun := createDagRun(getTestDate(), getTestDag())
-	jobFrame := dagRun.getJobFrame()
-	jobsClient := KUBECLIENT.BatchV1().Jobs(dagRun.DAG.Config.Namespace)
-
-	createdJob, err := jobsClient.Create(context.TODO(), &jobFrame, v1.CreateOptions{})
-	dagRun.Job = createdJob
-	if err != nil {
-		panic(err)
-	}
-	dagRun.deleteJob()
-	list, err := jobsClient.List(context.TODO(), v1.ListOptions{})
-	if err != nil {
-		panic(err)
-	}
-	for _, job := range list.Items {
-		if unmarshalJob(*createdJob) == unmarshalJob(job) {
-			t.Errorf("Job %s should have been deleted", createdJob.Name)
-		}
-	}
+func TestAddDagRunIfReady(t *testing.T) {
+	testDag := getTestDag()
+	panic(testDag)
 }
