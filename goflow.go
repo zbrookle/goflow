104b278cb07e6864881bba5c3573588abeb416dc
zbrookle <zachb1996@yahoo.com>
2020-12-04 15:01:43 -0500

    ENH: Clean up code for main program

--- goflow.go
+++ goflow.go
@@ -3,30 +3,23 @@ package main
 import (
 	"flag"
 	"goflow/dag/orchestrator"
-	k8sclient "goflow/k8s/client"
-	podutils "goflow/k8s/pod/utils"
-	"goflow/logs"
-	"goflow/testutils"
+	"goflow/paths"
 	"time"
 )
 
 func main() {
 	configPath := flag.String(
 		"path",
-		testutils.GetConfigPath(),
+		paths.GetGoDefaultHomePath(),
 		"The path to the configuration file",
 	)
 	flag.Parse()
 
-	kubeClient := k8sclient.CreateKubeClient()
-
-	defer podutils.CleanUpPods(kubeClient)
 	orch := *orchestrator.NewOrchestrator(*configPath)
 	loopBreaker := make(chan struct{}, 1)
-	go orch.Start(1, loopBreaker)
+	orch.Start(1*time.Second, loopBreaker)
 
-	time.Sleep(4 * time.Second)
-	loopBreaker <- struct{}{}
+	for {
 
-	logs.InfoLogger.Println("Dags length", len(orch.DAGs()))
+	}
 }
