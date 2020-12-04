6aa2980ac2c3530c35a90f49d4d566783bc0939d
zbrookle <zachb1996@yahoo.com>
2020-12-04 10:27:14 -0500

    ENH: Added func to set defaults on dag configs

--- dag/config/dag_config.go
+++ dag/config/dag_config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"encoding/json"
+	"goflow/config"
 
 	core "k8s.io/api/core/v1"
 )
@@ -38,4 +39,47 @@ func (config DAGConfig) Marshal() []byte {
 // JSON returns a json string representation of DAGConfig
 func (config DAGConfig) JSON() string {
 	return string(config.Marshal())
+}
+
+func makeStrMapCopy(src map[string]string) map[string]string {
+	if src == nil {
+		return nil
+	}
+	cpy := make(map[string]string)
+	for key := range src {
+		cpy[key] = src[key]
+	}
+	return cpy
+}
+
+// Copy returns a copy of the DAGConfig
+func (config DAGConfig) Copy() DAGConfig {
+	configCopy := config
+	configCopy.Command = make([]string, len(config.Command))
+	copy(configCopy.Command, config.Command)
+	configCopy.Annotations = makeStrMapCopy(config.Annotations)
+	configCopy.Labels = makeStrMapCopy(config.Labels)
+	return configCopy
+}
+
+// SetDefaults sets the defaults on the dag config using the goflow config settings
+func (config *DAGConfig) SetDefaults(goflowConfig config.GoFlowConfig) {
+	if config.DockerImage == "" {
+		config.DockerImage = goflowConfig.DefaultDockerImage
+	}
+	if config.Namespace == "" {
+		config.Namespace = goflowConfig.DefaultNamespace
+	}
+	if config.Parallelism == 0 {
+		config.Parallelism = goflowConfig.Parallelism
+	}
+	if config.TimeLimit == 0 {
+		config.TimeLimit = goflowConfig.TimeLimit
+	}
+	if config.Retries == 0 {
+		config.Retries = goflowConfig.Retries
+	}
+	if config.MaxActiveRuns == 0 {
+		config.MaxActiveRuns = goflowConfig.MaxActiveRuns
+	}
 }
---
+++ dag/config/dag_config_test.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"goflow/config"
+	"goflow/jsonpanic"
+	"testing"
+
+	core "k8s.io/api/core/v1"
+)
+
+func TestSetConfigDefaults(t *testing.T) {
+	goflowConfig := config.GoFlowConfig{
+		DefaultNamespace:     "default",
+		DefaultDockerImage:   "busybox",
+		DAGPath:              "path",
+		DateFormat:           "2019-01-01",
+		DefaultRestartPolicy: core.RestartPolicyNever,
+		Parallelism:          1,
+		TimeLimit:            1,
+		Retries:              1,
+		MaxActiveRuns:        1,
+	}
+	expectedDagConfig := DAGConfig{
+		Name:        "test-config",
+		Namespace:   goflowConfig.DefaultNamespace,
+		Schedule:    "* * * * *",
+		DockerImage: goflowConfig.DefaultDockerImage,
+		RetryPolicy: core.RestartPolicyNever,
+		Command: []string{
+			"echo",
+			"test",
+		},
+		Parallelism:   1,
+		TimeLimit:     1,
+		Retries:       1,
+		MaxActiveRuns: 1,
+		WithLogs:      false,
+	}
+	dagConfigCases := []DAGConfig{
+		{
+			Name:        expectedDagConfig.Name,
+			Schedule:    expectedDagConfig.Schedule,
+			RetryPolicy: core.RestartPolicyNever,
+			Command:     []string{"echo", "test"},
+			WithLogs:    false,
+		},
+		expectedDagConfig.Copy(),
+	}
+	for _, dagConfig := range dagConfigCases {
+		dagConfig.SetDefaults(goflowConfig)
+		configString := jsonpanic.JSONPanicFormat(dagConfig)
+		expectedString := jsonpanic.JSONPanicFormat(expectedDagConfig)
+		if configString != expectedString {
+			t.Errorf("Expected\n%s\nbut found\n%s", expectedString, configString)
+		}
+	}
+}
