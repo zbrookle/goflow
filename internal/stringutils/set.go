32ce3ef6a030e75bfd7a30869c362704d3cb5f34
zbrookle <zachb1996@yahoo.com>
2020-12-19 20:22:53 -0500

    ENH: Add string set tools

---
+++ internal/stringutils/set.go
@@ -0,0 +1,35 @@
+package stringutils
+
+import "fmt"
+
+// StringSet is a set of strings
+type StringSet map[string]struct{}
+
+// NewStringSet returns a new StringSet
+func NewStringSet(strings []string) StringSet {
+	set := make(StringSet)
+	for _, s := range strings {
+		set.Add(s)
+	}
+	return set
+}
+
+// Add adds a string to the set
+func (s StringSet) Add(str string) {
+	s[str] = struct{}{}
+}
+
+// Remove removes a string from the set
+func (s StringSet) Remove(str string) {
+	delete(s, str)
+}
+
+// GetOne returns a string from the set
+func (s StringSet) GetOne() (retString string, err error) {
+	retString = ""
+	err = fmt.Errorf("nothing to get")
+	for str := range s {
+		retString, err = str, nil
+	}
+	return
+}
