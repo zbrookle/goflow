527b778705a21111c7cb7bc82d1e8424b231c08b
zbrookle <zachb1996@yahoo.com>
2021-02-16 10:12:12 -0500

    ENH: Add REST api to main program

--- goflow.go
+++ goflow.go
@@ -6,9 +6,14 @@ import (
 	"goflow/internal/k8s/client"
 	"goflow/internal/k8s/pod/utils"
 	"goflow/internal/paths"
+	"goflow/internal/rest"
 	"goflow/internal/termination"
 	"time"
 )
+
+var host string
+var port int
+
 
 func main() {
 	defer utils.CleanUpEnvironment(client.CreateKubeClient())
@@ -17,12 +22,15 @@ func main() {
 		paths.GetGoDefaultHomePath(),
 		"The path to the configuration file",
 	)
+	host := flag.String("host", "localhost", "Host IP to serve REST api on")
+	port := flag.Int("port", 8080, "Port to serve REST API on")
 	flag.Parse()
 
-	orch := *orchestrator.NewOrchestrator(*configPath)
+	orch := orchestrator.NewOrchestrator(*configPath)
 	orch.Start(1 * time.Second)
 	go termination.Handle(func() {
 		orch.Stop()
 	})
+	go rest.Serve(*host, *port, orch)
 	orch.Wait()
 }
