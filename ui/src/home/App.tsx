8a78850158a0ff8cfb6bfe092a4763708299aec6
zbrookle <zachb1996@yahoo.com>
2021-02-19 20:46:08 -0500

    ENH: Switch DAGs view to table

--- ui/src/home/App.tsx
+++ ui/src/home/App.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import {DAGContainer} from "./dags";
+import { DAGContainer } from "./dags";
 
 function Header() {
   return (
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
-        <Navbar.Brand href="#home">GoFlow</Navbar.Brand>
-        <Navbar.Collapse>
-          <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#metrics">Metrics</Nav.Link>
-            <Nav.Link href="">Settings</Nav.Link>
-            <Nav.Link href="">Documentation</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+      <Navbar.Brand href="#home">GoFlow</Navbar.Brand>
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#metrics">Metrics</Nav.Link>
+          <Nav.Link href="">Settings</Nav.Link>
+          <Nav.Link href="">Documentation</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
--- ui/src/home/dags.tsx
+++ ui/src/home/dags.tsx
@@ -2,55 +2,87 @@ import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
 import Switch from "bootstrap-switch-button-react";
 
 const styles = {
-    row: {
-        padding: 8
-    },
-    col: {
-        padding: 3
-    }
-}
+  row: {
+    // padding: 8,
+  },
+  col: {
+    padding: 3,
+  },
+};
 
 const ColOffsets = {
-    Status: 2,
-    Name: 1,
-    RunTime: 0,
-    Successes: 4
-}
+  Status: 0,
+  Name: 0,
+  RunTime: 0,
+  Successes: 0,
+};
 
 type DAGProps = {
   Name: string;
 };
 
+function CenterColHead(props: any) {
+    return <th className="my-auto">{props.children}</th>
+}
+
+function CenteredCol(props: any) {
+  return <td className="my-auto">{props.children}</td>;
+}
+
+function DAGColumnHeaders() {
+  return (
+    <thead>
+      <tr style={styles.row}>
+        <CenterColHead>Status</CenterColHead>
+        <CenterColHead>Name</CenterColHead>
+        <CenterColHead>Last Run Time</CenterColHead>
+        <CenterColHead>Success/Failures</CenterColHead>
+      </tr>
+    </thead>
+  );
+}
+
 function DAG(props: DAGProps) {
   const [dagActive, switchDag] = useState(false);
-  var date = new Date()
-  const [lastRunTime] = useState(date.toISOString())
+  var date = new Date();
+  const [lastRunTime] = useState(date.toISOString());
 
   return (
-    <Row style={styles.row}>
-      <Col xs={ColOffsets.Status}>
+    <tr style={styles.row}>
+      <CenteredCol colOffset={ColOffsets.Status}>
         <Switch checked={dagActive} onChange={() => switchDag(!dagActive)} />
-      </Col>
-      <Col xs={ColOffsets.Name}>{props.Name}</Col>
-      <Col xs={ColOffsets.RunTime}>{lastRunTime}</Col>
-      <Col xs={ColOffsets.Successes}>Success/failures</Col>
-    </Row>
+      </CenteredCol>
+      <CenteredCol xs={ColOffsets.Name}>{props.Name}</CenteredCol>
+      <CenteredCol xs={ColOffsets.RunTime}>{lastRunTime}</CenteredCol>
+      <CenteredCol xs={ColOffsets.Successes}>Success/failures</CenteredCol>
+    </tr>
   );
 }
 
 function DAGContainer() {
-  let dags: Record<number, string> = {1: "test"}
-  for (var i = 0; i < 13; i ++) {
-     dags[i] = "test" + i.toString();
+  let dags: Record<number, string> = { 1: "test" };
+  for (var i = 0; i < 40; i++) {
+    dags[i] = "test" + i.toString();
   }
   return (
-    <Container>
-        { Object.entries(dags).map((t,k) => <DAG Name={t[1]} />) }          
-    </Container>
+    <div>
+      My DAGs
+      <p />
+      (Dag Search Bar Here)
+      <Table bordered variant="dark">
+        <DAGColumnHeaders />
+        <tbody>
+          {Object.entries(dags).map((t, k) => (
+            <DAG Name={t[1]} />
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
-export { DAGContainer }
+export { DAGContainer };
