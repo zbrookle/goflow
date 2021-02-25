516fa480711b92df909328126bfaef37475f65ad
zbrookle <zachb1996@yahoo.com>
2021-02-25 18:35:01 -0500

    ENH: Add header to page

--- ui/src/dag/dag_page.tsx
+++ ui/src/dag/dag_page.tsx
@@ -1,9 +1,21 @@
-import type { DAGProps } from "../typing/dag_types"
+import type { DAGProps } from "../typing/dag_types";
+import { Container, Row } from "react-bootstrap";
+import { OnOffButton } from "../buttons/on_off_button";
 
 type DagInfoProps = DAGProps & {
-  Test: boolean
+  JobName: string;
+  MaxMemoryUsage: number;
+  Successes: number;
+  Failures: number;
 };
 
-function DagInfo(props: DagInfoProps) {}
+function DagInfo(props: DagInfoProps) {
+  return (
+    <div>
+      <OnOffButton Name={props.Name} IsOn={props.IsOn} />
+      <h1 style={{ marginLeft: "1%" }}>{props.Name}</h1>
+    </div>
+  );
+}
 
 export { DagInfo };
