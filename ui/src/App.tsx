26baa5cfff210b01f9bf9616aa52a63b4fc83ba1
zbrookle <zachb1996@yahoo.com>
2021-02-25 17:03:14 -0500

    ENH: Set up navbar for router

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,18 +2,36 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { DAGContainer } from "./home/dags";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import { DagInfo } from "./dag/dag_page";
+
+type RouterNavLinkProps = {
+  link: string,
+  text: string
+};
+
+function RouterNavLink(props: RouterNavLinkProps) {
+  return (
+    <LinkContainer to={props.link}>
+      <Nav.Link>{props.text}</Nav.Link>
+    </LinkContainer>
+  );
+}
+
 function Header() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
-      <Navbar.Brand href="#home">GoFlow</Navbar.Brand>
+       <LinkContainer to={"/home"}>
+          <Navbar.Brand>GoFlow</Navbar.Brand>
+      </LinkContainer>
       <Navbar.Collapse>
         <Nav>
-          <Link to="/">Home</Link>
-          <Nav.Link href="#metrics">Metrics</Nav.Link>
-          <Nav.Link href="">Settings</Nav.Link>
-          <Link to="/dag">DAG</Link>
+          <RouterNavLink link={"/home"} text="Home" />
+          <RouterNavLink link={"/metrics"} text="Metrics" />
+          <RouterNavLink link={"/settings"} text="Settings" />
+          <RouterNavLink link={"/dag"} text="DAG" />
           <Nav.Link href="https://github.com/zbrookle/goflow">
             Documentation
           </Nav.Link>
@@ -31,6 +49,18 @@ function App() {
         <Switch>
           <Route path="/">
             <DAGContainer />
+          </Route>
+          <Route path="/">
+            <DagInfo
+              Name="test"
+              Schedule="* * * *"
+              LastRunTime="Never"
+              IsOn={false}
+              JobName={"test-run"}
+              MaxMemoryUsage={20}
+              Successes={10}
+              Failures={5}
+            />
           </Route>
         </Switch>
       </div>
