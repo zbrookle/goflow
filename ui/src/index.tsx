b80c5822073e08780c63266929a8ccdd6130e833
zbrookle <zachb1996@yahoo.com>
2021-02-25 16:17:12 -0500

    ENH: Add router for switching pages

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./home/App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Head } from "./bootstrap_cdn";
