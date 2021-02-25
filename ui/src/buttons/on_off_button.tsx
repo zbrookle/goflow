c8ff38fcf00fdb1749eb93b780430a4062808405
zbrookle <zachb1996@yahoo.com>
2021-02-25 18:35:17 -0500

    ENH: Create on/off as separate component

---
+++ ui/src/buttons/on_off_button.tsx
@@ -0,0 +1,37 @@
+import Switch from "bootstrap-switch-button-react";
+import { useLayoutEffect, useState } from "react";
+
+type OnOffButtonProps = {
+  Name: string;
+  IsOn: boolean;
+};
+
+export function OnOffButton(props: OnOffButtonProps) {
+  const toggleRequestOptions = {
+    method: "PUT",
+  };
+  const [isOn, setButtonOn] = useState(false);
+  function getDAGIsOn() {
+    const fetchURL = `http://localhost:8080/dag/${props.Name}`
+    fetch(fetchURL)
+      .then((resp) => resp.json())
+      .then((data) => setButtonOn(data.IsOn));
+  }
+  useLayoutEffect(getDAGIsOn, [props.Name]);
+  return (
+    <Switch
+      size="sm"
+      checked={isOn}
+      onChange={() => {
+        fetch(
+          `http://localhost:8080/dag/${props.Name}/toggle`,
+          toggleRequestOptions
+        ).then(
+          resp => resp.json()
+        ).then(
+          (data) => setButtonOn(data.IsOn)
+        );
+      }}
+    />
+  );
+}
