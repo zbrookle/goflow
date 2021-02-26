a95d8a1e67943a8bb1b4ce5a12565824b7f49cf3
zbrookle <zachb1996@yahoo.com>
2021-02-26 16:52:51 -0500

    ENH: Add path routing to dag card

--- ui/src/dag/dag_page.tsx
+++ ui/src/dag/dag_page.tsx
@@ -1,7 +1,7 @@
 import type { DAGProps } from "../typing/dag_types";
 import { Container, Row, Col, Card, Nav } from "react-bootstrap";
 import { OnOffButton } from "../buttons/on_off_button";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route, useRouteMatch } from "react-router-dom";
 import { RouterNavLink } from "../routing/router_nav";
 
 type DagInfoProps = DAGProps & {
@@ -11,20 +11,29 @@ type DagInfoProps = DAGProps & {
   Failures: number;
 };
 
+function getPath(path: string, name: string) {
+  return `${path}/${name}`;
+}
+
 type CardTabProps = {
   Ref: string;
   Label: string;
+  Path: string;
 };
 
 function CardTab(props: CardTabProps) {
+  let link = getPath(props.Path, props.Ref);
+
   return (
     <Nav.Item>
-      <RouterNavLink link={props.Ref} text={props.Label} />
+      <RouterNavLink link={link} text={props.Label} />
     </Nav.Item>
   );
 }
 
 function DagInfo(props: DagInfoProps) {
+  let { path, url } = useRouteMatch();
+
   return (
     <Container style={{ marginTop: "1%" }}>
       <Row noGutters={true}>
@@ -45,14 +54,27 @@ function DagInfo(props: DagInfoProps) {
       <Row>
         <Card>
           <Card.Header>
-            <Nav variant="tabs" defaultActiveKey="#metrics">
-              <CardTab Ref="#metrics" Label="Metrics" />
-              <CardTab Ref="#timeline" Label="Timeline" />
-              <CardTab Ref="#runtimes" Label="Run Times" />
-              <CardTab Ref="#resources" Label="Resource Usage" />
+            <Nav variant="tabs" defaultActiveKey={getPath(url, "metrics")}>
+              <CardTab Path={url} Ref="metrics" Label="Metrics" />
+              <CardTab Path={url} Ref="timeline" Label="Timeline" />
+              <CardTab Path={url} Ref="runtimes" Label="Run Times" />
+              <CardTab Path={url} Ref="resources" Label="Resource Usage" />
             </Nav>
           </Card.Header>
-          <Card.Body>Hello!</Card.Body>
+          <Switch>
+            <Route exact path={getPath(path, "metrics")}>
+              <Card.Body>Metrics!</Card.Body>
+            </Route>
+            <Route path={getPath(path, "timeline")}>
+              <Card.Body>Timeline!</Card.Body>
+            </Route>
+            <Route path={getPath(path, "runtimes")}>
+              <Card.Body>Run run run!</Card.Body>
+            </Route>
+            <Route path={getPath(path, "resources")}>
+              <Card.Body>mems and cps!</Card.Body>
+            </Route>
+          </Switch>
           <Card.Footer className="text-muted">Last Updated:</Card.Footer>
         </Card>
       </Row>
