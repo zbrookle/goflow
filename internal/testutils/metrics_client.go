18b12cf05974efe553ae81c4a45557f5b74c5465
zbrookle <zachb1996@yahoo.com>
2021-02-28 20:07:50 -0500

    ENH: Start creating metrics listener

--- internal/testutils/metrics_client.go
+++ internal/testutils/metrics_client.go
@@ -1,13 +1,46 @@
 package testutils
 
 import (
+	"context"
+	"fmt"
 	"goflow/internal/dag/metrics"
 
+	k8sapi "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 	fakemetrics "k8s.io/metrics/pkg/client/clientset/versioned/fake"
 )
 
+func registerPodsIntoMetrics(
+	kubeClient *fake.Clientset,
+	metricsClient *fakemetrics.Clientset,
+) {
+	namespaces, err := kubeClient.CoreV1().Namespaces().List(context.TODO(), k8sapi.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for true {
+		for _, namespace := range namespaces.Items {
+			podList, err := kubeClient.CoreV1().Pods(
+				namespace.Name,
+			).List(
+				context.TODO(),
+				k8sapi.ListOptions{},
+			)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(podList)
+		}
+	}
+	// kubeClient.CoreV1().Pods()
+	// metricsClient.Tracker().Add()
+}
+
 // NewTestMetricsClient returns a new metrics client for testing only
-func NewTestMetricsClient() *metrics.DAGMetricsClient {
+func NewTestMetricsClient(interfaces ...*fake.Clientset) *metrics.DAGMetricsClient {
 	fakeMetricsClientSet := fakemetrics.NewSimpleClientset()
+	for _, inter := range interfaces {
+		go registerPodsIntoMetrics(inter, fakeMetricsClientSet)
+	}
 	return metrics.NewDAGMetricsClient(fakeMetricsClientSet)
 }
